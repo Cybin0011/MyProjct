090fb73c0e51e9f7f16645dd9bda3a1c9334a13b
Cybin0011 <13281856736@163.com>
2022-06-14 23:17:10 +0800

    Initial commit

---
+++ Mvn_Mybatis/src/main/java/com/Cybin/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.Cybin.mapper;
+
+import com.Cybin.pojo.User;
+
+import java.util.List;
+
+/**
+ * @author Chen Yuanbin
+ * @description TODO
+ * @create 2022-06-14 22:24
+ */
+public interface UserMapper {
+    List<User> selectAll();
+}
